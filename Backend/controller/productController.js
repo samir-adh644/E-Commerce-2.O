1665a76e9ddfbf66a1c2d55d44cc9ae6e52665c8
Samir Adhikari <081bct071@ioepc.edu.np>
2026-01-24 21:18:16 +0545

    made backedn for displaying singleProduct

--- Backend/controller/productController.js
+++ Backend/controller/productController.js
@@ -32,3 +32,26 @@ exports.displayProduct = async(req,res)=>{
     }
   
 }
+
+exports.displaySingleProduct = async (req,res)=>{
+    const {id} = req.params
+
+        try{
+          const productData = await products.findOne({where:{id:id}})
+
+           if (!productData) {
+      return res.status(404).json({
+        success: false,
+        message: "Product not found",
+      });
+    }
+    
+          return res.status(200).json({success: true,
+            data:productData,
+          });
+    } catch(err){
+        return res.status(500).json({message:"Data Not found"})
+    }
+
+
+}
--- Backend/routes/productRoutes.js
+++ Backend/routes/productRoutes.js
@@ -1,4 +1,4 @@
-const { uploadProduct, displayProduct } = require('../controller/productController')
+const { uploadProduct, displayProduct, displaySingleProduct } = require('../controller/productController')
 const multer = require("multer")
 const { storage } = require("../middleware/multerConfig") // whatever you named it
 const upload = multer({ storage })
@@ -8,5 +8,6 @@ const router = require('express').Router()
 
 router.route('/upload-product').post(upload.single("image"),uploadProduct)
 router.route('/renderProduct').get(displayProduct)
+router.route('/product/:id').get(displaySingleProduct)
 
 module.exports = router ;
